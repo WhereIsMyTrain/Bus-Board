ade2f5cac26e20270ff438c4f4d23b6e22ae59d1
Benjamin Boyle <benjaminboyle9@gmail.com>
2013-08-25 01:31:23 +1000

    google play library added

--- Busboard/src/com/whereismytrain/transitboard/Map.java
+++ Busboard/src/com/whereismytrain/transitboard/Map.java
@@ -5,14 +5,17 @@ import com.whereismytrain.transitboard.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
-public class Map extends Activity {
+public class Map extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 	}
+    
 	
 
 
