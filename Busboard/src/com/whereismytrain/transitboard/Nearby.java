c6e363145edf3ff724bd336428a5d52928a19443
Michael Murphy <michael.murphy4@uqconnect.edu.au>
2013-08-30 09:18:52 +1000

    comments and cleanup

--- Busboard/src/com/whereismytrain/transitboard/Nearby.java
+++ Busboard/src/com/whereismytrain/transitboard/Nearby.java
@@ -16,48 +16,52 @@ public class Nearby extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nearby);
 		jsonp jParser = new jsonp();
+		// Using a hardcoded url for simplicity for the tech spike
 		String url = "http://deco3801-003.uqcloud.net/opia/location/rest/stops-nearby/AD:300%20sir%20fred%20schonell?radiusM=500&useWalkingDistance=true&maxResults=20";
 		 
-		// getting JSON string from URL
+		// Gets the JSON from the url
 		JSONObject json = jParser.getJSONFromUrl(url);
 		try {
+			//Splits the JSON file in each individual stop 
 			JSONArray stops = json.getJSONArray("NearbyStops");
-//					TextView textName = (TextView) findViewById(R.id.textView2);
-			LinearLayout A = (LinearLayout) findViewById(R.id.nearby);
 			 for(int i = 0; i < stops.length(); i++){
 				JSONObject c = stops.getJSONObject(i);
+				//Gets the StopId in the format 'XXXXXX'
 				String id = c.getString("StopId");
 				url = "http://deco3801-003.uqcloud.net/opia/location/rest/stops?ids=SI%3A"
 						+ id;
 				JSONObject stopDetails = jParser.getJSONFromUrl(url);
 				JSONObject stopDetail = stopDetails.getJSONArray("Stops")
 						.getJSONObject(0);
-				String street = stopDetail.getString("Description");
-				String position = stopDetail.getString("Position");
+				//Gets relevant details for each stop
+				String description = stopDetail.getString("Description");
+				//String position = stopDetail.getString("Position");
 				String zone = stopDetail.getString("Zone");
+				//Gets the distance from the selected loction to the stop
 				JSONObject d = c.getJSONObject("Distance");
 				String distance = d.getString("DistanceM");
-			 	LinearLayout lineOne = new LinearLayout(this);
-			 	lineOne.setOrientation(LinearLayout.HORIZONTAL);
-				TextView test = new TextView(this);
-				test.setText("Stop: "+street);
-				lineOne.addView(test, 0);
-			 	LinearLayout lineTwo = new LinearLayout(this);
-			 	lineTwo.setOrientation(LinearLayout.HORIZONTAL);
-			 	lineTwo.setPadding(0, 0, 0, 8);
-				TextView test2 = new TextView(this);
-				test2.setText("Distance: "+distance+" metres");
-				lineTwo.addView(test2, 0);
-				TextView test3 = new TextView(this);
-				test3.setText("Zone: "+zone);
-				test3.setPadding(8, 0, 8, 0);
-				lineTwo.addView(test3, 1);
-				A.addView(lineOne);
-				A.addView(lineTwo);
+				//Builds the layout for stop details on two lines
+				//Whitespace tabs used to show nesting of layouts
+				LinearLayout masterLayout = (LinearLayout) findViewById(R.id.nearby);
+				 	LinearLayout lineOne = new LinearLayout(this);
+						TextView descriptionText = new TextView(this);
+						descriptionText.setText("Stop: "+description);
+					lineOne.addView(descriptionText);
+				 	LinearLayout lineTwo = new LinearLayout(this);
+				 	lineTwo.setOrientation(LinearLayout.HORIZONTAL);
+				 	//Add a bottom margin to separate from the next stops details
+				 	lineTwo.setPadding(0, 0, 0, 10);
+						TextView distanceText = new TextView(this);
+						distanceText.setText("Distance: "+distance+" metres");
+						TextView zoneText = new TextView(this);
+						zoneText.setText("Zone: "+zone);
+						zoneText.setPadding(8, 0, 0, 0);
+					lineTwo.addView(distanceText, 0);
+					lineTwo.addView(zoneText, 1);
+				masterLayout.addView(lineOne);
+				masterLayout.addView(lineTwo);
 			 }
-//					textName.setText(String.valueOf(contacts.length()));
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
--- Busboard/src/com/whereismytrain/transitboard/jsonp.java
+++ Busboard/src/com/whereismytrain/transitboard/jsonp.java
@@ -5,24 +5,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
- 
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
- 
+
 import android.os.StrictMode;
-import android.util.Log;
  
 public class jsonp {
  
     static InputStream is = null;
-    static JSONObject jObj = null;
+    static JSONObject JSON = null;
     static String json = "";
     
  
@@ -32,50 +28,34 @@ public class jsonp {
     }
  
     public JSONObject getJSONFromUrl(String url) {
+    	//Uses the UI thread for HTTP access. Will change this to a Async method.
     	StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
     	StrictMode.setThreadPolicy(policy); 
-        // Making HTTP request
+        // Gets the http response and puts it in a input stream
         try {
-            // defaultHttpClient
             DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpGet httpPost = new HttpGet(url);
- 
-            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpResponse httpResponse = httpClient.execute(new HttpGet(url));
             HttpEntity httpEntity = httpResponse.getEntity();
             is = httpEntity.getContent();          
- 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-         
+        //BufferReader to read the input stream and to out put it into a string
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            json = reader.readLine();
+            reader.close();
             is.close();
-            json = sb.toString();
         } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
+        	e.printStackTrace();
         }
- 
-        // try parse the string to a JSON object
+    	//Convert the string into a JSON object.
         try {
-            jObj = new JSONObject(json);
-        } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
-        }
- 
-        // return JSON String
-        return jObj;
+			JSON = new JSONObject(json);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+        return JSON;
  
     }
 }
