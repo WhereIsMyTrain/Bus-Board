96351b5993cb00b00ea9027afb15e43b99187cbf
BenjaminBoyle <benjaminboyle9@gmail.com>
2013-09-02 17:20:39 +1000

    empty test project

---
+++ transitboard_tests/src/com/whereismytrain/transitboard/test/MainScreenTest.java
@@ -0,0 +1,26 @@
+package com.whereismytrain.transitboard.test;
+
+import com.whereismytrain.transitboard.HomeScreen;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainScreenTest extends ActivityInstrumentationTestCase2<HomeScreen> {
+
+	public MainScreenTest(Class<HomeScreen> activityClass) {
+		super(activityClass);
+		// TODO Auto-generated constructor stub
+	}
+	public MainScreenTest() {  
+		super(HomeScreen.class);  
+		}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
