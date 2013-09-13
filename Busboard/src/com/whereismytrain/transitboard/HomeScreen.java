01d94a95200e3e2e77cae5b517c62477e3eecf59
BenjaminBoyle <benjaminboyle9@gmail.com>
2013-09-13 14:04:45 +1000

    bug fix on From

--- Busboard/src/com/whereismytrain/transitboard/HomeScreen.java
+++ Busboard/src/com/whereismytrain/transitboard/HomeScreen.java
@@ -4,13 +4,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.whereismytrain.transitboard.R;
 
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
 import android.view.View;
 import android.widget.*;
@@ -38,6 +41,8 @@ import android.util.Log;
 
 
 public class HomeScreen extends Activity {
+	
+	private Location userLocation;
 
 	//JSON Parser should spurt something like this out:
 	public static Object[][] busstops = {
@@ -50,11 +55,10 @@ public class HomeScreen extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
 		TextView or = (TextView)findViewById(R.id.Or);
-		
-		Location location = getUserLocation();
+		getUserLocation();
 		
 		try {
-			bindNearbyStops(location);
+			bindNearbyStops(userLocation);
 		} catch (NullPointerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -92,7 +96,6 @@ public class HomeScreen extends Activity {
 		//murphy.michael
 		//0f82-5eU585N
 		//AD:300%20sir%20fred%20schonell?
-		location = getUserLocation();
 		String latitude = String.valueOf(location.getLatitude());
 		String longitude = String.valueOf(location.getLongitude());
 		
@@ -107,37 +110,62 @@ public class HomeScreen extends Activity {
 		Spinner from = (Spinner)findViewById(R.id.From);
 		List<String> list = new ArrayList<String>();
 		
-		for(int i = 0; i < stops.length(); i++){
-			JSONObject c = stops.getJSONObject(i);
-			//Gets the StopId in the format 'XXXXXX'
-			String id = c.getString("StopId");
-			url = "http://deco3801-003.uqcloud.net/opia/location/rest/stops?ids=SI%3A"
-					+ id;
-			//Look to move this step to Server side to reduce the number of HTTP request the clients makes
-			JSONObject stopDetails = jParser.getJSONFromUrl(url);
-			JSONObject stopDetail = stopDetails.getJSONArray("Stops")
-					.getJSONObject(0);
-			//Gets relevant details for each stop
-			String description = stopDetail.getString("Description");
-			//String position = stopDetail.getString("Position");
-			String zone = stopDetail.getString("Zone");
-			//Gets the distance from the selected location to the stop
-			JSONObject d = c.getJSONObject("Distance");
-			String distance = d.getString("DistanceM");
-			//Builds the layout for stop details on two lines
-			//Whitespace tabs used to show nesting of layouts
-			
-			list.add(description + ": " + distance + "m, zone: " + zone);
-		 }
+		if (userLocation != null) {
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-				this, R.layout.textview, list);
-		adapter.setDropDownViewResource(R.layout.textview);
-		from.setAdapter(adapter);
+			for(int i = 0; i < stops.length(); i++){
+				JSONObject c = stops.getJSONObject(i);
+				//Gets the StopId in the format 'XXXXXX'
+				String id = c.getString("StopId");
+				url = "http://deco3801-003.uqcloud.net/opia/location/rest/stops?ids=SI%3A"
+						+ id;
+				//Look to move this step to Server side to reduce the number of HTTP request the clients makes
+				JSONObject stopDetails = jParser.getJSONFromUrl(url);
+				JSONObject stopDetail = stopDetails.getJSONArray("Stops")
+						.getJSONObject(0);
+				//Gets relevant details for each stop
+				String description = stopDetail.getString("Description");
+				//String position = stopDetail.getString("Position");
+				String zone = stopDetail.getString("Zone");
+				//Gets the distance from the selected location to the stop
+				JSONObject d = c.getJSONObject("Distance");
+				String distance = d.getString("DistanceM");
+				//Builds the layout for stop details on two lines
+				//Whitespace tabs used to show nesting of layouts
+				
+				list.add(description + ": " + distance + "m, zone: " + zone);
+			 }
+			
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+					this, R.layout.textview, list);
+			adapter.setDropDownViewResource(R.layout.textview);
+			from.setAdapter(adapter);
+		}
 	}
 	
-	public Location getUserLocation() {
-		Location location;
+	LocationListener locationListener = new LocationListener() {
+	    public void onLocationChanged(Location location) {
+	      // Called when a new location is found by the network location provider.
+	      userLocation = location;
+	      try {
+			bindNearbyStops(userLocation);
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      
+	    }
+
+	    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+	    public void onProviderEnabled(String provider) {}
+
+	    public void onProviderDisabled(String provider) {}
+	  };
+
+	public void getUserLocation() {
 		Boolean isGPSEnabled;
 		Boolean isNetworkEnabled;
 		//LatLng location;
@@ -148,19 +176,16 @@ public class HomeScreen extends Activity {
 		isNetworkEnabled = locationManager
                 .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-		location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		/*
-		if (isGPSEnabled) {
-			location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		} else if (isNetworkEnabled) {
-			location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-		} else
-			location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-		*/
-		Toast.makeText(getApplicationContext(), location.getLatitude() + ", " + location.getLongitude() , Toast.LENGTH_LONG).show();
-
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 		
-		return location;
+		if (userLocation == null)
+			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+		if (userLocation == null)
+			locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 0, 0, locationListener);
+			//location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+		
+		if (userLocation != null)
+			Toast.makeText(getApplicationContext(), userLocation.getLatitude() + ", " + userLocation.getLongitude() , Toast.LENGTH_LONG).show();
 	}
 	//open map
 	public void mapScreen(View view) {
